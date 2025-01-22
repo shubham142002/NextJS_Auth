965a6abe6796edf4fd557050c8c123832a1ca138
Shubham Patel <shubhamparam123@gmail.com>
2025-01-22 13:39:36 +0530

    Add email verification page and update mailer functionality
    - Implemented a new VerifyEmailPage component for user email verification.
    - Added logic to handle token extraction and verification API call.
    - Updated sendEmail function to improve code formatting and change email sender address.
    - Enhanced error handling in email verification process.

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -3,32 +3,38 @@ import User from "@/models/userModel";
 import bcryptjs from 'bcryptjs';
 
 
-export const sendEmail = async({email, emailType, userId}:any) => {
+export const sendEmail = async ({ email, emailType, userId }: any) => {
     try {
         // create a hased token
         const hashedToken = await bcryptjs.hash(userId.toString(), 10)
 
         if (emailType === "VERIFY") {
-            await User.findByIdAndUpdate(userId, 
-                {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000})
-        } else if (emailType === "RESET"){
-            await User.findByIdAndUpdate(userId, 
-                {forgotPasswordToken: hashedToken, forgotPasswordTokenExpiry: Date.now() + 3600000})
+            await User.findByIdAndUpdate(userId,
+                {
+                    verifyToken: hashedToken,
+                    verifyTokenExpiry: Date.now() + 3600000
+                })
+        } else if (emailType === "RESET") {
+            await User.findByIdAndUpdate(userId,
+                {
+                    forgotPasswordToken: hashedToken,
+                    forgotPasswordTokenExpiry: Date.now() + 3600000
+                })
         }
 
+        // Looking to send emails in production? Check out our Email API/SMTP product!
         var transport = nodemailer.createTransport({
             host: "sandbox.smtp.mailtrap.io",
             port: 2525,
             auth: {
-              user: "3fd364695517df",
-              pass: "7383d58fd399cf"
-              //TODO: add these credentials to .env file
+                user: "b944ebfac7fcb8",
+                pass: "678989d17f56b7"
             }
-          });
+        });
 
 
         const mailOptions = {
-            from: 'hitesh@gmail.com',
+            from: '201240107031ce@gmail.com',
             to: email,
             subject: emailType === "VERIFY" ? "Verify your email" : "Reset your password",
             html: `<p>Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "VERIFY" ? "verify your email" : "reset your password"}
@@ -37,10 +43,10 @@ export const sendEmail = async({email, emailType, userId}:any) => {
         }
 
         const mailresponse = await transport.sendMail
-        (mailOptions);
+            (mailOptions);
         return mailresponse;
 
-    } catch (error:any) {
+    } catch (error: any) {
         throw new Error(error.message);
     }
 }
